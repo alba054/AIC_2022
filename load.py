9a033c636ac2f5677c5bb750cf2c30fa70070ec6
alba054 <jralif02@gmail.com>
2022-09-03 19:21:38 +0800

    add zones for each animals

--- load.py
+++ load.py
@@ -1,12 +1,13 @@
 import csv
 import json
+import sys
 
-from utils import get_database
+from utils_api import get_database
 
 clientDB = get_database()
-CSV_PATH = '../data/animal_detail.csv'
 
-if __name__ == "__main__":
+def load_csv_to_database():
+  CSV_PATH = '../data/animal_detail.csv'
   animals = []
   with open(CSV_PATH) as csvfile:
     reader = csv.DictReader(csvfile)
@@ -15,3 +16,34 @@ if __name__ == "__main__":
   
   animalCollection = clientDB["animals"]
   animalCollection.insert_many(animals)
+
+def insert_document(path_to_document):
+  with open(path_to_document) as jsonfile:
+    zones = json.load(jsonfile)
+  
+  locationsCollection = clientDB["locations"]
+  locationsCollection.insert_many(zones)
+
+def update_document(path_to_document):
+  with open(path_to_document) as jsonfile:
+    updates = json.load(jsonfile)
+  
+  animalsCollection = clientDB["animals"]
+  for update in updates:
+    animalsCollection.update_one(update['data'], update['updated'])
+
+if __name__ == "__main__":
+  if sys.argv[1] == "insert":
+    if len(sys.argv) < 3:
+      print("provide path")
+    else:
+      insert_document(sys.argv[2])
+  elif sys.argv[1] == "update":
+    if len(sys.argv) < 3:
+      print("provide path")
+    else:
+      update_document(sys.argv[2])
+
+
+  # print(sys.argv)
+  # insert_document("data/locations.json")
--- main.py
+++ main.py
@@ -2,15 +2,14 @@ from flask import Flask, request
 import json
 
 from utils_api import get_database
+'''
+for production
+'''
 from detect import load_image_and_detect
 
 
 app = Flask(__name__)
 clientDB = get_database()
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
 
 @app.route("/animals/<ids>")
 def getAnimalById(ids):
@@ -18,14 +17,43 @@ def getAnimalById(ids):
   id_list = set(id_list)
   animalCollection = clientDB["animals"]
   response = []
-  for id in id_list:  
-    animal = animalCollection.find({"id" : id})
-    animal = list(animal)[0]
-    del animal["_id"]
-    response.append(animal)
+  try:
+    for id in id_list:
+        animal = animalCollection.find({"id" : id})
+        animal = list(animal)
+        del animal["_id"]
+        response.append(animal)
+  except:
+    return { "results" : [] }
 
   return { "results" : response }
 
+@app.route("/locations/<id>")
+def getLocationByID(id):
+  locationCollection = clientDB["locations"]
+  animalsCollection = clientDB["animals"]
+  try:
+    location = locationCollection.find({"id" : int(id)})
+    location = list(location)[0]
+    del location["_id"]
+    
+    filtered_animals = animalsCollection.find({"zones" : {"$in" : [int(id)] } })
+    animals = []
+
+    for animal in filtered_animals:
+      animals.append(animal['id'])
+
+    return { "result" : {
+      "destination" : location,
+      "animals" : animals
+      } 
+    }
+  except:
+    return { "result" : {} }
+
+'''
+for production
+'''
 @app.route("/detect", methods=['POST'])
 def detect():
   body = json.loads(request.data)
